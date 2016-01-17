3f32656d9578b5d83b020af1770c1a9ab65a812a
KwonKyungsoon <bh3kks@naver.com>
2016-01-18 02:11:40 +0900

    views.py변경사항 1. isinstance 추가 2. 페이지 수 계산해서 배열로 넘겨줌 3. try-except로 예외처리 4. prev_entry, next_entry추가 후 read.html로 넘김

--- blog/views.py
+++ blog/views.py
@@ -10,18 +10,33 @@ from django.template import Context
 def index(request, page=1):
 	# 글 목록을 보여주는 함수
 	# 기본적인 request말고도 page도 인자로 넘겨받는다(기본값 = 1)
+	if isinstance(page, int) == False:
+		page = 1
+	# page가 적절한 int 값으로 들어왔는지 검사
+
 	page_title = '블로그 글 목록 화면'
 
+	# 페이지 수 계산
 	per_page = 5
 	start_pos = (int(page)-1)*per_page
 	end_pos = start_pos+per_page
+
+	res = Entries.objects.count()/5
+	if (Entries.objects.count()%5) > 0:
+		remain = 1
+	else:
+		remain = 0
+	page_num = int(res) + int(remain) + 1
+	# page_num은 실제 페이지 수보다 +1
 
 	entries = Entries.objects.all().order_by('-created')[start_pos:end_pos]
  	# Entries.objects.all()로 모든 Entries를 부러오고, 한 페이지에 5개씩 보여진다
 	context = {
 		'page_title':page_title,
 		'entries':entries,
-		'current_page':page
+		'current_page':page,
+		'range':range(page_num)
+		#range로 0,1,2,3... 배열 생성 -> 전달
 	}
 
 	return render(request, 'index.html', context)
@@ -33,6 +48,23 @@ def read(request, entry_id=None):
 
 	current_entry = Entries.objects.get(id=int(entry_id))
 
-	return HttpResponse('안녕, 여러분. 여긴 [%s]이고, [%d]번째 글의 제목은 [%s], 내용은 [%s]이야.' 
-		% (page_title, current_entry.id, current_entry.Title, current_entry.Content))
+	try:
+		prev_entry = current_entry.get_previous_by_created()
+	except:
+		prev_entry = None
+
+	try:
+		next_entry = current_entry.get_next_by_created()
+	except:
+		next_entry = None
+
+
+	context = {
+		'page_title':page_title,
+		'current_entry':current_entry,
+		'prev_entry':prev_entry,
+		'next_entry':next_entry
+	}
+
+	return render(request, 'read.html', context)
 
